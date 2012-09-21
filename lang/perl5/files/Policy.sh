6af196471a6a71761d12127a952a6b42cf961da1
Jonathan Perkin <jperkin@joyent.com>
2012-09-21 15:36:07 +0100

    Fix multiarch library paths.

--- lang/perl5/files/Policy.sh
+++ lang/perl5/files/Policy.sh
@@ -97,7 +97,7 @@ lddlflags="@LOCLIBRPATHFLAGS@ \$lddlflags"
 # Set pkgsrc defaults for library and header search paths:
 # nail down the directories in which headers and libraries of
 # locally-installed software may be found.
-loclibpth="@LOCALBASE@/lib"
+loclibpth="@LOCALBASE@/lib@LIBARCHSUFFIX@"
 locincpth="@LOCALBASE@/include"
 
 # Set pkgsrc defaults for "plateform"/general path used to 
